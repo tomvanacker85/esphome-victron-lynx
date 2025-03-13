16e901b78578ce5f0974178326675b625a22b22b
tomvanacker85 <49290930+tomvanacker85@users.noreply.github.com>
2025-03-13 11:42:04 +0100

    Update victron-lynx.h

--- components/victron-lynx/victron-lynx.h
+++ components/victron-lynx/victron-lynx.h
@@ -1,1 +1,26 @@
+#include "esphome.h"
+#include "Wire.h"
+
+#define LYNX_I2C_ADDRESS 0x40  // Adjust if necessary
+
+class VictronLynx : public PollingComponent, public Sensor {
+ public:
+  VictronLynx() : PollingComponent(5000) {}
+
+  void setup() override {
+    Wire.begin();
+  }
+
+  void update() override {
+    Wire.beginTransmission(LYNX_I2C_ADDRESS);
+    Wire.write(0x00);  // Example command to read fuse states
+    Wire.endTransmission();
+
+    Wire.requestFrom(LYNX_I2C_ADDRESS, 1);
+    if (Wire.available()) {
+      int fuse_state = Wire.read();
+      publish_state(fuse_state);
+    }
+  }
+};
 
